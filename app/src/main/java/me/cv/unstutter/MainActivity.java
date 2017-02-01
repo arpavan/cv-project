6e7a95280392ef9d0285f0021cc899a5ea4bf887
Pavan Agnihotri <arpavan1990@gmail.com>
2017-01-31 22:10:07 -0500

    Configured OpenCV libs into project - OpenCV 3.2 for Android

--- app/src/main/java/me/cv/unstutter/MainActivity.java
+++ app/src/main/java/me/cv/unstutter/MainActivity.java
@@ -8,6 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        System.loadLibrary("opencv_java3");
+
         setContentView(R.layout.activity_main);
     }
 }
