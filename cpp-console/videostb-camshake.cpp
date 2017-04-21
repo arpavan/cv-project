5d9d71c59dcc33c6622906c804ab92572a32800c
Pavan Agnihotri <arpavan1990@gmail.com>
2017-04-21 05:17:48 -0400

    some tweaks

--- cpp-console/videostb-camshake.cpp
+++ cpp-console/videostb-camshake.cpp
@@ -7,10 +7,12 @@
 using namespace std;
 using namespace cv;
 
-struct TransformParam
+const int SMOOTHING_RADIUS = 50; // size of the fixed bounds to determine the degree of stabilization effect. Higher the better.
+
+struct Transformation
 {
-    TransformParam() {}
-    TransformParam(double _dx, double _dy, double _da) {
+    Transformation() {}
+    Transformation(double _dx, double _dy, double _da) {
         dx = _dx;
         dy = _dy;
         da = _da;
@@ -53,7 +55,7 @@ int main(int argc, char **argv)
 
     string outFile;
     stringstream outFilestream;
-    outFilestream << argv[1] << "Processed" << ".avi";
+    outFilestream << argv[1] << "Temp" << ".avi";
     outFile = outFilestream.str();
     Size S = Size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH), (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT));    // Acquire input size
     int fourcc = CV_FOURCC('H','2','6','4');
@@ -73,13 +75,13 @@ int main(int argc, char **argv)
     cap >> prev;
     cvtColor(prev, prev_grey, COLOR_BGR2GRAY);
 
-    // Step 1 - Get previous to current frame transformation (dx, dy, da) for all frames
-    vector <TransformParam> prev_to_cur_transform; // previous to current
+    vector <Transformation> prev_to_cur_transform; // previous to current
 
     int k=1;
     int max_frames = cap.get(CV_CAP_PROP_FRAME_COUNT);
     Mat last_T, lastGoodFrame;
 
+    // pass-1: Process the entire video to elimiate jerks.
     while(true) {
         cap >> cur;
 
@@ -102,7 +104,7 @@ int main(int argc, char **argv)
         // Calculate optical flow using Lucas-Kanade
         calcOpticalFlowPyrLK(prev_grey, cur_grey, prev_corner, cur_corner, status, err);
 
-        // weed out bad matches // status = status vector stores 1/0 depending on the detection of flow for the given set of features
+        // eliminate any zero matches // status = status vector stores 1/0 depending on the detection of flow for the given set of features
         for(size_t i=0; i < status.size(); i++) {
             if(status[i]) {
                 prev_corner2.push_back(prev_corner[i]);
@@ -117,7 +119,6 @@ int main(int argc, char **argv)
         double dy = -1;
         double da = -1;
 
-        // TODO: DONT COPY
         if(T.data != NULL) {
             //write these frames to new video
 
@@ -149,11 +150,134 @@ int main(int argc, char **argv)
         cout << "dy transform " << dy << endl;
         
         // getting the previous-to-current transformation for all frames
-        prev_to_cur_transform.push_back(TransformParam(dx, dy, da));
+        prev_to_cur_transform.push_back(Transformation(dx, dy, da));
 
-        cout << "Frame: " << k << "/" << max_frames << " - good optical flow: " << prev_corner2.size() << endl;
         k++;
     }
-
+    outputVideo.release();
     cap.release();
+    //Pass-1 ends.
+
+    // Pre-pass-2: Computing and manipulating the transformations between frames to prepare for stabilization
+    // from Pass-1 compute the transformation between all frames and compute the trajectory of the features through the frames.
+    // get transformations between frames
+    double a = 0;
+    double x = 0;
+    double y = 0;
+
+    vector <Trajectory> trajectory; // trajectory at all frames
+
+    for(size_t i=0; i < prev_to_cur_transform.size(); i++) {
+        x += prev_to_cur_transform[i].dx;
+        y += prev_to_cur_transform[i].dy;
+        a += prev_to_cur_transform[i].da;
+
+        trajectory.push_back(Trajectory(x,y,a));
+
+    }    
+
+    // smooth out the trajectory computed above using predetermined bounds.
+    vector <Trajectory> smoothed_trajectory; // trajectory at all frames
+
+    for(size_t i=0; i < trajectory.size(); i++) {
+        double sum_x = 0;
+        double sum_y = 0;
+        double sum_a = 0;
+        int count = 0;
+
+        for(int j=-SMOOTHING_RADIUS; j <= SMOOTHING_RADIUS; j++) {
+            if(i+j >= 0 && i+j < trajectory.size()) {
+                sum_x += trajectory[i+j].x;
+                sum_y += trajectory[i+j].y;
+                sum_a += trajectory[i+j].a;
+
+                count++;
+            }
+        }
+
+        double avg_a = sum_a / count;
+        double avg_x = sum_x / count;
+        double avg_y = sum_y / count;
+
+        smoothed_trajectory.push_back(Trajectory(avg_x, avg_y, avg_a));
+
+    }
+
+    // Compute the "new" frame-to-frame transformations to apply to the video frameset. These newly computed transformations
+    // stabilize the video for vibrations
+    vector <Transformation> new_prev_to_cur_transform;
+    a = 0;
+    x = 0;
+    y = 0;
+
+    for(size_t i=0; i < prev_to_cur_transform.size(); i++) {
+        x += prev_to_cur_transform[i].dx;
+        y += prev_to_cur_transform[i].dy;
+        a += prev_to_cur_transform[i].da;
+
+        double diff_x = smoothed_trajectory[i].x - x;
+        double diff_y = smoothed_trajectory[i].y - y;
+        double diff_a = smoothed_trajectory[i].a - a;
+
+        double dx = prev_to_cur_transform[i].dx + diff_x;
+        double dy = prev_to_cur_transform[i].dy + diff_y;
+        double da = prev_to_cur_transform[i].da + diff_a;
+
+        new_prev_to_cur_transform.push_back(Transformation(dx, dy, da));
+
+    }
+
+    VideoCapture cap2(outFile);
+    if(!cap2.isOpened())  // if not success, exit program
+    {
+        cout << "cap2 Cannot open file" << endl;
+        exit(0);
+    }
+
+    // Final pass - Iterate through all the frames and apply the above computed transformations
+    // Apply affine warping with the computed transformations.
+    cap2.set(CV_CAP_PROP_POS_FRAMES, 0);
+    Mat T(2,3,CV_64F);
+
+	string outFile2;
+    stringstream outFilestream2;
+    outFilestream2 << argv[1] << "Processed" << ".avi";
+    outFile2 = outFilestream2.str();
+	outputVideo.open(outFile2, fourcc, 30, S, true);
+
+
+    k=0;
+    while(k < max_frames-1) { // we cant get a valid transform for the last frame so skip it
+        cap2 >> cur;
+
+        if(cur.data == NULL) {
+            break;
+        }
+
+        // extracting the updated transformation matrix (rotation, translation)
+        T.at<double>(0,0) = cos(new_prev_to_cur_transform[k].da);
+        T.at<double>(0,1) = -sin(new_prev_to_cur_transform[k].da);
+        T.at<double>(1,0) = sin(new_prev_to_cur_transform[k].da);
+        T.at<double>(1,1) = cos(new_prev_to_cur_transform[k].da);
+
+        T.at<double>(0,2) = new_prev_to_cur_transform[k].dx;
+        T.at<double>(1,2) = new_prev_to_cur_transform[k].dy;
+
+
+        Mat cur2;
+
+        // warp the current frame based on the computed transformation
+        warpAffine(cur, cur2, T, cur.size());
+
+        waitKey(20);
+
+        k++;
+
+        outputVideo << cur2;
+    }
+
+    cap2.release();
+    outputVideo.release();
+
+    return 0;
 }
